1eec2f9d534d2f9218a540afadf4c0e12ddef53f
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-11-06 00:19:47 +0530

    add logout add change header depending on user login

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,39 +4,75 @@ import { auth } from "../services/firebase";
 
 function Header() {
   return (
-    <header className='border-bottom py-4 px-3 fixed-top bg-white d-flex justify-content-between align-items-center'>
+    <header className="border-bottom py-4 px-3 fixed-top bg-white d-flex justify-content-between align-items-center">
       <p>Live Chat Support</p>
       <div>
-        <span className='pl-3'><Search /></span>
-        <span className='pl-3'><Person /></span>
-        <span className='pl-3'><Dot /></span>
-
+        <span className="pl-3">
+          <Search />
+        </span>
+        <span className="pl-3">
+          <Person />
+        </span>
+        <span className="pl-3">
+          <Dot />
+        </span>
+        {auth().currentUser ? (
+          <div>
+            <Link to="/chat">Profile</Link>
+            <button onClick={() => auth().signOut()}>Logout</button>
+          </div>
+        ) : (
+          <>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
       </div>
     </header>
   );
 }
 
-
 function Search() {
-  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-    <path fillRule="evenodd" d="M11.5 7a4.499 4.499 0 11-8.998 0A4.499 4.499 0 0111.5 7zm-.82 4.74a6 6 0 111.06-1.06l3.04 3.04a.75.75 0 11-1.06 1.06l-3.04-3.04z">
-    </path>
-  </svg>
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      width="16"
+      height="16"
+    >
+      <path
+        fillRule="evenodd"
+        d="M11.5 7a4.499 4.499 0 11-8.998 0A4.499 4.499 0 0111.5 7zm-.82 4.74a6 6 0 111.06-1.06l3.04 3.04a.75.75 0 11-1.06 1.06l-3.04-3.04z"
+      ></path>
+    </svg>
+  );
 }
 
-
 function Dot() {
-  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-    <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z">
-    </path>
-  </svg>
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      width="16"
+      height="16"
+    >
+      <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+    </svg>
+  );
 }
 
 function Person() {
-  return <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z" />
-  </svg>
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z" />
+    </svg>
+  );
 }
 
-export default Header
+export default Header;
